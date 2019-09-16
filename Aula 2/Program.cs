6a9873d7157f8d5689f3b4b9cf5b6b2d5af63dfa
FIC <FIC@info.local>
2019-09-16 12:02:19 -0300

    Adicionado primeira estrutura condicional

---
+++ Aula 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaramos as notas que utilizaremos
+            double nota1, nota2, nota3, media;
+
+            //capturamos a primeira nota
+            Console.WriteLine("Digite sua primeira nota: ");
+            nota1 = double.Parse(Console.ReadLine() );
+
+            //capturamos a segunda nota
+            Console.WriteLine("Digite sua segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine() );
+
+            //capturamos a terceira nota
+            Console.WriteLine("Digite sua terceira nota: ");
+            nota3 = double.Parse(Console.ReadLine() );
+
+            ;
+            //calculamos a média das notas
+            media = (nota1 + nota2 + nota3) / 3;
+
+            //verificamos se a media do aluno é maior ou igual à
+            if(media >= 6){
+                
+                Console.WriteLine($"Média: {media} Aluno aprovado!");
+            }else{
+                 Console.WriteLine($"Média {media}Aluno reprovado");
+            }
+            
+        }
+    }
+}
