0c3c22254f3cd55aeccce6a35e576b34b9ce4c27
FIC <FIC@info.local>
2019-09-16 14:14:53 -0300

    criando condiçao

---
+++ Aula2-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deixamos declarada como falsa por padrão
+            bool entrada = false;
+            int idade;
+            string retorno;
+
+            Console.Write("Digite sua idade ");
+            idade = int.Parse(Console.ReadLine() );
+
+            //Como ja tribuimos anteriormente, mudamos o 
+            //estada da variável para verdadeira caso
+            //entre nessa condição
+            if(idade > 17){
+                entrada = true;
+            }
+            if( entrada == true){
+                retorno = "Entrada permitida!!!";
+            }else{
+                retorno = "Entrada não permitida :(";
+            }
+            Console.WriteLine(retorno);
+        }
+    }
+}
